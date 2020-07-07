b1b48882657d1fead44a177fe1d1da46103807ff
Andrey Kruglov <kruglov.it@gmail.com>
2020-07-07 03:24:23 +0300

    Queries and Mutations

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
